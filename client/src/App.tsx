ff14cf5df09f2f18309198c1bf25c4e15e49889e
frankskyblue <su.kong@mail.utoronto.ca>
2024-02-02 03:51:43 -0500

    Ready for deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+    const url = "http://" + process.env.REACT_APP_BACKEND_API_DOMAIN;
+    console.log(url)
+    const [message , setMessage] = React.useState<string>('');
+    useEffect(() => {
+        axios.get(url + '/api/hello')
+            .then((res) => {
+                console.log(res.data)
+                setMessage(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
   return (
       <h1 className="text-3xl font-bold underline text-red-600">
-        App Page
+          {message}
       </h1>
   );
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,23 @@
-import express, { Express }  from 'express';
+import express, {Express, Router} from 'express';
+import cors from 'cors';
+
+const corsConfig = {
+    origin: [process.env.REACT_FRONTEND_URL!, process.env.REACT_FRONTEND_URL_WWW!],
+    credentials: true
+}
 
 const app: Express = express()
+const router: Router = express.Router()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello from backend!')
+app.use(cors())
+
+router.get('/hello', (req, res) => {
+    res.send('Hello from the backend!')
 })
+
+// app.use(cors(corsConfig))
+app.use('/api', router)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
