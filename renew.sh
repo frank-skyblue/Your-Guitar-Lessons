c5e090b01d1705025a46d2c16d1e26367f133e0c
frank-skyblue <su.kong@mail.utoronto.ca>
2024-02-12 11:37:50 -0500

    Changed domain name to co

--- renew.sh
+++ renew.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo certbot certonly --webroot -w ./certbot/www/ --config-dir ./certbot/conf/ --email=frank.st.kong@gmail.com --agree-tos --no-eff-email -d yourguitarlessons.tech
+sudo certbot certonly --webroot -w ./certbot/www/ --config-dir ./certbot/conf/ --email=frank.st.kong@gmail.com --agree-tos --no-eff-email -d yourguitarlessons.co -d www.yourguitarlessons.co
