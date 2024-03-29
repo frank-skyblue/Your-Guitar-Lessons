e12f278d8a2f95389ec5989361edfd2b6114c9af
frank-skyblue <su.kong@mail.utoronto.ca>
2024-03-28 22:01:01 -0400

    Refactoring for maintainability

--- client/src/routes/adminCalendar.tsx
+++ client/src/routes/adminCalendar.tsx
@@ -22,127 +22,152 @@ const calendarEvents = getMockCalendarEvents();
 const Calendar: React.FC = () => {
   return (
     <div className="h-full p-2 gap-y-2 md:gap-y-0 md:px-10 md:py-5 flex flex-col">
-      <div className="basis-12 flex flex-col gap-y-2 md:gap-y-0 md:flex-row justify-between">
-        <div className="flex gap-x-2 justify-center items-center">
-          <PlayIcon className="w-4 h-4 rotate-180" />
-          <div className="text-xl font-semibold">
-            {months[new Date().getMonth()]} {new Date().getFullYear()}
-          </div>
-          <PlayIcon className="w-4 h-4" />
+      <Header />
+      <CalendarContents />
+      <CalendarContentsMobile />
+      <AddEventButton />
+    </div>
+  );
+};
+
+const Header = () => {
+  const date = new Date();
+  return (
+    <div className="basis-12 flex flex-col gap-y-2 md:gap-y-0 md:flex-row justify-between">
+      <div className="flex gap-x-2 justify-center items-center">
+        <PlayIcon className="w-4 h-4 rotate-180" />
+        <div className="text-xl font-semibold">
+          {months[date.getMonth()]} {date.getFullYear()}
         </div>
-        <div className="flex gap-x-3 justify-center items-center">
-          <div className="text-xs w-24 text-center bg-past-highlight py-1 rounded-md">
-            Past
-          </div>
-          <div className="text-xs w-24 text-center bg-booked-highlight py-1 rounded-md">
-            Booked
-          </div>
-          <div className="text-xs w-24 text-center bg-available-highlight py-1 rounded-md">
-            Available
-          </div>
-        </div>
+        <PlayIcon className="w-4 h-4" />
       </div>
-      <div className="hidden grow bg-content-highlight md:flex flex-col border-2 border-b-0 border-text-primary/15">
-        {/* Headers */}
-        <div className="basis-10 flex divide-x-2 divide-text-primary/15">
-          {days.map((day) => (
-            <div
-              key={day}
-              className="h-full basis-0 grow text-sm font-medium flex items-center border-b-2 border-text-primary/15"
-            >
-              <div className="ml-2">{day}</div>
-            </div>
-          ))}
+      <div className="flex gap-x-3 justify-center items-center">
+        <div className="text-xs w-24 text-center bg-past-highlight py-1 rounded-md">
+          Past
         </div>
-        {/* Dates Container */}
-        <div className="h-full flex flex-col">
-          {Array.from(
-            { length: calendarEvents.length > 35 ? 6 : 5 },
-            (_, i) => (
-              /* Rows */
-              <div
-                key={i}
-                className="basis-0 h-0 grow flex divide-x-2 divide-text-primary/15 overflow-y-hidden"
-              >
-                {calendarEvents
-                  .slice(i * 7, i * 7 + 7)
-                  .map(({ date, events }) => (
-                    // Date Box
-                    <div
-                      key={date > 0 ? date : Math.random()}
-                      className="h-full basis-0 grow text-sm font-normal flex flex-col border-b-2 border-text-primary/15 overflow-y-hidden"
-                    >
-                      {/* Date */}
-                      <div>
-                        {date > 0 ? (
-                          <div className="ml-2">{date}</div>
-                        ) : (
-                          <div className="h-full w-full bg-text-inactive/10"></div>
-                        )}
-                      </div>
-                      {/* Events */}
-                      <div className="mt-auto flex flex-col gap-y-0.5 overflow-y-scroll">
-                        {events.map(({ type, time, address }) => (
-                          <div
-                            key={time}
-                            className={`${
-                              type === "past"
-                                ? "bg-past-highlight"
-                                : type === "booked"
-                                ? "bg-booked-highlight"
-                                : "bg-available-highlight"
-                            } border-l-4 border-text-primary/50 px-0.5 py-1.5`}
-                          >
-                            <div className="text-xs font-semibold">{time}</div>
-                            {address && (
-                              <div className="text-xs">{address}</div>
-                            )}
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  ))}
-              </div>
-            )
-          )}
+        <div className="text-xs w-24 text-center bg-booked-highlight py-1 rounded-md">
+          Booked
         </div>
-      </div>
-      <div className="md:hidden grow flex flex-col gap-y-3 overflow-y-scroll">
-        {calendarEvents.map(
-          ({ date, events }) =>
-            events.length > 0 && (
-              <div key={date} className="flex flex-col gap-y-1">
-                {/* Date */}
-                <div className="text-sm font-normal text-center border-b-2 border-text-primary/15">
-                  {date}
-                </div>
-                {/* Events */}
-                <div className="flex flex-col gap-y-0.5 overflow-y-scroll">
-                  {events.map(({ type, time, address }) => (
-                    <div
-                      key={time}
-                      className={`${
-                        type === "past"
-                          ? "bg-past-highlight"
-                          : type === "booked"
-                          ? "bg-booked-highlight"
-                          : "bg-available-highlight"
-                      } flex justify-between border-l-4 border-text-primary/50 px-1 py-1.5`}
-                    >
-                      <div className="text-xs font-semibold">{time}</div>
-                      {address && <div className="text-xs">{address}</div>}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )
-        )}
-      </div>
-      <div className="md:hidden basis-12 flex justify-center items-center bg-text-primary/35 rounded-md">
-        <PlusIcon className="w-6 h-6" />
+        <div className="text-xs w-24 text-center bg-available-highlight py-1 rounded-md">
+          Available
+        </div>
       </div>
     </div>
   );
 };
+
+const CalendarContents = () => {
+  const Header = () => (
+    <div className="basis-10 flex divide-x-2 divide-text-primary/15">
+      {days.map((day) => (
+        <div
+          key={day}
+          className="h-full basis-0 grow text-sm font-medium flex items-center border-b-2 border-text-primary/15"
+        >
+          <div className="ml-2">{day}</div>
+        </div>
+      ))}
+    </div>
+  );
+
+  const Date = ({ date }: any) =>
+    date > 0 ? (
+      <div className="ml-2">{date}</div>
+    ) : (
+      <div className="h-full w-full bg-text-inactive/10"></div>
+    );
+
+  const Events = ({ events }: { events: any }) =>
+    events.map(({ type, time, address }: any) => (
+      <div
+        key={time}
+        className={`${
+          type === "past"
+            ? "bg-past-highlight"
+            : type === "booked"
+            ? "bg-booked-highlight"
+            : "bg-available-highlight"
+        } border-l-4 border-text-primary/50 px-0.5 py-1.5`}
+      >
+        <div className="text-xs font-semibold">{time}</div>
+        {address && <div className="text-xs">{address}</div>}
+      </div>
+    ));
+
+  return (
+    <div className="hidden grow bg-content-highlight md:flex flex-col border-2 border-b-0 border-text-primary/15">
+      <Header />
+      {/* Dates Container */}
+      <div className="h-full flex flex-col">
+        {Array.from({ length: calendarEvents.length > 35 ? 6 : 5 }, (_, i) => (
+          /* Rows */
+          <div
+            key={i}
+            className="basis-0 h-0 grow flex divide-x-2 divide-text-primary/15 overflow-y-hidden"
+          >
+            {calendarEvents.slice(i * 7, i * 7 + 7).map(({ date, events }) => (
+              // Date Box
+              <div
+                key={date > 0 ? date : Math.random()}
+                className="h-full basis-0 grow text-sm font-normal flex flex-col border-b-2 border-text-primary/15 overflow-y-hidden"
+              >
+                {/* Date */}
+                <Date date={date} />
+                <div className="mt-auto flex flex-col gap-y-0.5 overflow-y-scroll">
+                  <Events events={events} />
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const CalendarContentsMobile = () => {
+  const Events = ({ events }: { events: any }) =>
+    events.map(({ type, time, address }: any) => (
+      <div
+        key={time}
+        className={`${
+          type === "past"
+            ? "bg-past-highlight"
+            : type === "booked"
+            ? "bg-booked-highlight"
+            : "bg-available-highlight"
+        } flex justify-between border-l-4 border-text-primary/50 px-1 py-1.5 rounded-sm`}
+      >
+        <div className="text-xs font-semibold">{time}</div>
+        {address && <div className="text-xs">{address}</div>}
+      </div>
+    ));
+
+  return (
+    <div className="md:hidden grow flex flex-col gap-y-3 overflow-y-scroll">
+      {calendarEvents.map(
+        ({ date, events }) =>
+          events.length > 0 && (
+            <div key={date} className="flex flex-col gap-y-1">
+              {/* Date */}
+              <div className="text-sm font-normal text-center border-b-2 border-text-primary/15">
+                {date}
+              </div>
+              {/* Events */}
+              <div className="flex flex-col gap-y-0.5 overflow-y-scroll">
+                <Events events={events} />
+              </div>
+            </div>
+          )
+      )}
+    </div>
+  );
+};
+
+const AddEventButton = () => (
+  <div className="md:hidden basis-12 flex justify-center items-center bg-text-primary/35 rounded-md">
+    <PlusIcon className="w-6 h-6" />
+  </div>
+);
 
 export default Calendar;
--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,123 +1,131 @@
-import React, { useEffect, useState } from 'react';
-import { getMockPreviousLessons, getMockUpcomingLessons } from '../mock/getMockData';
-import { PreviousLessonsObjType, UpcomingLessonsObjType } from '../util/types';
+import React, { useEffect, useState } from "react";
+import {
+  getMockPreviousLessons,
+  getMockUpcomingLessons,
+} from "../mock/getMockData";
+import { PreviousLessonsObjType, UpcomingLessonsObjType } from "../util/types";
 
 const Home: React.FC = () => {
-    return (
-        <div className='h-full flex flex-col gap-y-3 md:gap-y-0 md:flex-row md:gap-x-6 p-3 md:px-6 md:py-8'>
-            <div className='basis-3/4 md:basis-3/5 flex flex-col gap-y-3 md:gap-y-6 overflow-hidden'>
-                <div className='basis-1/3 md:basis-2/6 flex flex-col gap-y-2 overflow-hidden text-center md:text-left'>
-                    <h1 className='text-2xl'>Upcoming Lessons</h1>
-                    <div className='md:bg-content-highlight flex-grow flex flex-col gap-y-2 rounded-md px-2 overflow-hidden'>
-                        <UpcomingLessons />
-                    </div>
-                </div>
-                <div className='basis-2/3 md:basis-4/6 flex flex-col gap-y-2 overflow-hidden text-center md:text-left'>
-                    <h1 className='text-2xl'>Previous Lessons</h1>
-                    <div className='md:bg-content-highlight flex-grow flex flex-col gap-y-2 rounded-md px-2 overflow-hidden'>
-                        <PreviousLessons />
-                    </div>
-                </div>
-            </div>
-            <div className='basis-1/4 md:basis-2/5 flex flex-col gap-y-2 order-first md:order-none text-center md:text-left'>
-                <h1 className='text-2xl'>Dashboard</h1>
-                <div className='md:bg-content-highlight flex-grow rounded-md'>
-                    <Dashboard />
-                </div>
-            </div>
+  return (
+    <div className="h-full flex flex-col gap-y-3 md:gap-y-0 md:flex-row md:gap-x-6 p-3 md:px-6 md:py-8">
+      <div className="basis-3/4 md:basis-3/5 flex flex-col gap-y-3 md:gap-y-6 overflow-hidden">
+        <div className="basis-1/3 md:basis-2/6 flex flex-col gap-y-2 overflow-hidden text-center md:text-left">
+          <h1 className="text-2xl">Upcoming Lessons</h1>
+          <div className="md:bg-content-highlight flex-grow flex flex-col gap-y-2 rounded-md px-2 overflow-hidden">
+            <UpcomingLessons />
+          </div>
         </div>
-    );
+        <div className="basis-2/3 md:basis-4/6 flex flex-col gap-y-2 overflow-hidden text-center md:text-left">
+          <h1 className="text-2xl">Previous Lessons</h1>
+          <div className="md:bg-content-highlight flex-grow flex flex-col gap-y-2 rounded-md px-2 overflow-hidden">
+            <PreviousLessons />
+          </div>
+        </div>
+      </div>
+      <div className="basis-1/4 md:basis-2/5 flex flex-col gap-y-2 order-first md:order-none text-center md:text-left">
+        <h1 className="text-2xl">Dashboard</h1>
+        <div className="md:bg-content-highlight flex-grow rounded-md">
+          <Dashboard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-const UpcomingLessonsHeaders = [
-    'Day',
-    'Date',
-    'Time',
-    'Location',
-    ''
-];
+const UpcomingLessonsHeaders = ["Day", "Date", "Time", "Location", ""];
 
 const UpcomingLessons: React.FC = () => {
-    const [upcomingLessons, setUpcomingLessons] = useState<UpcomingLessonsObjType[]>([]);
+  const [upcomingLessons, setUpcomingLessons] = useState<
+    UpcomingLessonsObjType[]
+  >([]);
 
-    useEffect(() => {
-        const mockUpcomingLessons: UpcomingLessonsObjType[] = getMockUpcomingLessons();
-        setUpcomingLessons(mockUpcomingLessons);
-    }
-        , []);
+  useEffect(() => {
+    const mockUpcomingLessons: UpcomingLessonsObjType[] =
+      getMockUpcomingLessons();
+    setUpcomingLessons(mockUpcomingLessons);
+  }, []);
 
-    if (upcomingLessons.length === 0) {
-        return (
-            <p>No upcoming lessons</p>
-        );
-    }
+  if (upcomingLessons.length === 0) {
+    return <p>No upcoming lessons</p>;
+  }
 
-    return (
-        <>
-            <div className='flex flex-row'>
-                {UpcomingLessonsHeaders.map((title, index) => (
-                    <p key={index} className='text-md font-bold basis-1/5'>{title}</p>
-                ))}
-            </div>
-            <div className='flex flex-col gap-y-2 overflow-auto'>
-            {upcomingLessons.map((lesson: UpcomingLessonsObjType, index) => (
-                <div key={index} className='flex flex-row'>
-                    {Object.values(lesson).map((value, index) => (
-                        <p key={index} className='text-md text-text-primary/60 basis-1/5'>{value}</p>
-                    ))}
-                </div>
+  return (
+    <>
+      <div className="flex flex-row">
+        {UpcomingLessonsHeaders.map((title, index) => (
+          <p key={index} className="text-md font-bold basis-1/5">
+            {title}
+          </p>
+        ))}
+      </div>
+      <div className="flex flex-col gap-y-2 overflow-auto">
+        {upcomingLessons.map((lesson: UpcomingLessonsObjType, index) => (
+          <div key={index} className="flex flex-row">
+            {Object.values(lesson).map((value, index) => (
+              <p key={index} className="text-md text-text-primary/60 basis-1/5">
+                {value}
+              </p>
             ))}
-            </div>
-        </>
-    );
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
-const PreviousLessonsHeaders = [
-    'Date',
-    'Description',
-    'Downloadable',
-];
+const PreviousLessonsHeaders = ["Date", "Description", "Downloadable"];
 
 const PreviousLessons: React.FC = () => {
-    const [previousLessons, setPreviousLessons] = useState<PreviousLessonsObjType[]>([]);
+  const [previousLessons, setPreviousLessons] = useState<
+    PreviousLessonsObjType[]
+  >([]);
 
-    useEffect(() => {
-        const mockPreviousLessons: PreviousLessonsObjType[] = getMockPreviousLessons();
-        setPreviousLessons(mockPreviousLessons);
-    }, []);
+  useEffect(() => {
+    const mockPreviousLessons: PreviousLessonsObjType[] =
+      getMockPreviousLessons();
+    setPreviousLessons(mockPreviousLessons);
+  }, []);
 
-    if (previousLessons.length === 0) {
-        return (
-            <p>No previous lessons</p>
-        );
-    }
+  if (previousLessons.length === 0) {
+    return <p>No previous lessons</p>;
+  }
 
-    return (
-        <>
-            <div className='flex flex-row'>
-                {PreviousLessonsHeaders.map((title, index) => (
-                    <p key={index} className='text-md font-bold first:basis-1/5 basis-2/6'>{title}</p>
-                ))}
-            </div>
-            <div className='flex flex-col gap-y-2 overflow-auto'>
-            {previousLessons.map((lesson: PreviousLessonsObjType, index) => (
-                <div key={index} className='flex flex-row'>
-                    {Object.values(lesson).map((value, index) => (
-                        <p key={index} className='text-md text-text-primary/60 first:basis-1/5 basis-2/6'>{value}</p>
-                    ))}
-                </div>
+  return (
+    <>
+      <div className="flex flex-row">
+        {PreviousLessonsHeaders.map((title, index) => (
+          <p
+            key={index}
+            className="text-md font-bold first:basis-1/5 basis-2/6"
+          >
+            {title}
+          </p>
+        ))}
+      </div>
+      <div className="flex flex-col gap-y-2 overflow-auto">
+        {previousLessons.map((lesson: PreviousLessonsObjType, index) => (
+          <div key={index} className="flex flex-row">
+            {Object.values(lesson).map((value, index) => (
+              <p
+                key={index}
+                className="text-md text-text-primary/60 first:basis-1/5 basis-2/6"
+              >
+                {value}
+              </p>
             ))}
-            </div>
-        </>
-    );
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 const Dashboard: React.FC = () => {
-    return (
-        <div>
-            <p>Dashboard</p>
-        </div>
-    );
+  return (
+    <div>
+      <p>Dashboard</p>
+    </div>
+  );
 };
 
 export default Home;
